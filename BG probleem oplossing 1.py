2c64de8d9fdbe0c872f1a87f6a59ed0ff34d4f56
gerritvanos <gerrit.vanos@student.hu.nl>
2017-11-07 12:26:59 +0100

    windows klaar buttons werkend

--- BG probleem oplossing 1.py
+++ BG probleem oplossing 1.py
@@ -1,9 +1,10 @@
-import requests
-import xmltodict
+#Onderstaande modules zijn nodig voor verschillende onderdelen van de code
+import requests         #Deze module wordt gebruikt voor het opvragen van de API
+import xmltodict        #Deze module wordt gebtuikt voor het omzetten van de gegevens naar een dict
 import sys
 from tkinter import *
 
-stations = ['Hilversum','Kampen', 'Culemborg', 'Utrecht Lunetten']
+stations = ['Hilversum','Kampen', 'Culemborg', 'Utrecht Lunetten', 'Driebergen-Zeist', 'Utrecht Centraal']
 def gegevens_opvragen(stationsNaam):
     auth_details = ('gerrit.vanos@student.hu.nl', 'BE66_yqU3kQWgKSQJdWPxEHZ8JFji7pM74B9fTCwQo1yZW3clRSQ4w')
     api_url = 'http://webservices.ns.nl/ns-api-avt?station='+stationsNaam
@@ -30,11 +31,12 @@ def info(station):
     Label(master=infoWindow, text='EindBestemming').grid(row=1, column=0, ipadx=10, ipady=10, )
     Label(master=infoWindow, text='Vertrekspoor').grid(row=1, column=1, ipadx=10, ipady=10, )
     Label(master=infoWindow, text='Vertrektijd').grid(row=1, column=2, ipadx=10, ipady=10, )
-    Label(master=infoWindow, text='Treinsoort').grid(row=1, column=3, ipadx=10, ipady=10, )
-    Label(master=infoWindow, text='Vervoerder').grid(row=1, column=4, ipadx=10, ipady=10, )
-    Label(master=infoWindow, text='Routetekst').grid(row=1, column=5, ipadx=10, ipady=10, )
-    Label(master=infoWindow, text='Routetips').grid(row=1, column=6, ipadx=10, ipady=10, )
-    Label(master=infoWindow, text='Opmerkingen').grid(row=1, column=7, ipadx=10, ipady=10, )
+    Label(master=infoWindow, text='Vertraging').grid(row=1, column=3, ipadx=10, ipady=10, )
+    Label(master=infoWindow, text='Treinsoort').grid(row=1, column=4, ipadx=10, ipady=10, )
+    Label(master=infoWindow, text='Vervoerder').grid(row=1, column=5, ipadx=10, ipady=10, )
+    Label(master=infoWindow, text='Routetekst').grid(row=1, column=6, ipadx=10, ipady=10, )
+    Label(master=infoWindow, text='Routetips').grid(row=1, column=7, ipadx=10, ipady=10, )
+    Label(master=infoWindow, text='Opmerkingen').grid(row=1, column=8, ipadx=10, ipady=10, )
 
     count =0
     for trein in gegevens:
@@ -48,29 +50,35 @@ def info(station):
                 kolom = 1
             elif key == 'VertrekTijd':
                 kolom = 2
-            elif key == 'TreinSoort':
+            elif key == 'VertrekVertraging':
                 kolom = 3
-            elif key == 'Vervoerder':
+            elif key == 'TreinSoort':
                 kolom = 4
-            elif key == 'RouteTekst':
+            elif key == 'Vervoerder':
                 kolom = 5
-            elif key == 'ReisTip':
+            elif key == 'RouteTekst':
                 kolom = 6
-            elif key == 'Opmerkingen':
+            elif key == 'ReisTip':
                 kolom = 7
-            elif key == 'VertrekVertraging' or key == 'VertrekVertragingTekst':
+            elif key == 'Opmerkingen':
+                kolom = 8
+            elif key == 'VertrekVertragingTekst':
                 continue
             if kolom == 1:
                 Label(master=infoWindow, text='{}'.format(trein[key]['#text'])).grid(row=rij, column=kolom, ipadx=10, ipady=10)
+            elif kolom == 2:
+                tijd = str(trein[key])
+                tijd = tijd[11:16]
+                Label(master=infoWindow, text='{}'.format(tijd)).grid(row=rij, column=kolom, ipadx=10, ipady=10)
             else:
                 Label(master=infoWindow, text='{}'.format(trein[key])).grid(row=rij, column=kolom, ipadx=10, ipady=10)
         rij += 1
         count +=1
 
-    gadoor = Button(master=infoWindow, text='ga door', command=keuze, bg='#000066', fg='white', font=('Frutiger Bold Regular', 40, 'bold'))
-    gadoor.grid(pady=10)
     gaterug = Button(master=infoWindow, text='ga terug', command=stoppen, bg='#000066', fg='white', font=('Frutiger Bold Regular', 40, 'bold'))
-    gaterug.grid(pady=10)
+    gaterug.grid(pady=10, padx=10,column=3,  row=rij)
+    gadoor = Button(master=infoWindow, text='ga door', command=choice, bg='#000066', fg='white', font=('Frutiger Bold Regular', 40, 'bold'))
+    gadoor.grid(pady=10, padx=10,  column=4, row=rij)
     infoWindow.mainloop()
 
 
@@ -80,7 +88,7 @@ def controleer():
 def stoppen():
     sys.exit()
 
-def keuze():
+def choice():
     try:
         root.destroy()
     except:
@@ -101,7 +109,7 @@ def keuze():
 root = Tk()
 root['bg']='#fed339'
 
-gadoor = Button(master=root, text='ga door', command=keuze, bg='#000066',fg='white', font=('Frutiger Bold Regular',40,'bold'))
+gadoor = Button(master=root, text='ga door', command=choice, bg='#000066',fg='white', font=('Frutiger Bold Regular',40,'bold'))
 gadoor.grid(pady=10)
 gaterug = Button(master=root, text='ga terug', command=stoppen, bg='#000066',fg='white', font=('Frutiger Bold Regular',40,'bold'))
 gaterug.grid(pady=10)
