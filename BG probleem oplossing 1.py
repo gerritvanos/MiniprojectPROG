e9688d4b058066a483a55c39bbc9a5ec1dd2f504
gerritvanos <gerrit.vanos@student.hu.nl>
2017-11-07 13:15:22 +0100

    Frame voor informatie gemaakt

--- BG probleem oplossing 1.py
+++ BG probleem oplossing 1.py
@@ -1,10 +1,11 @@
 #Onderstaande modules zijn nodig voor verschillende onderdelen van de code
 import requests         #Deze module wordt gebruikt voor het opvragen van de API
 import xmltodict        #Deze module wordt gebtuikt voor het omzetten van de gegevens naar een dict
-import sys
-from tkinter import *
+import sys              #Deze module wordt gebruikt om het programma af te kunnen sluiten
+from tkinter import *   #Deze module wordt gebruikt voor de GUI
 
-stations = ['Hilversum','Kampen', 'Culemborg', 'Utrecht Lunetten', 'Driebergen-Zeist', 'Utrecht Centraal']
+stations = ['Hilversum','Kampen', 'Culemborg', 'Utrecht Lunetten', 'Driebergen-Zeist', 'Utrecht Centraal'] #Deze lijst is de lijst met stations die opgevraagd kunnen worden
+
 def gegevens_opvragen(stationsNaam):
     auth_details = ('gerrit.vanos@student.hu.nl', 'BE66_yqU3kQWgKSQJdWPxEHZ8JFji7pM74B9fTCwQo1yZW3clRSQ4w')
     api_url = 'http://webservices.ns.nl/ns-api-avt?station='+stationsNaam
@@ -21,22 +22,26 @@ def info(station):
     rij = 2
     global infoWindow
     infoWindow = Tk()
-    infoWindow["bg"]='#fed339'
-    infoWindow.option_add('*Label.Background', '#fed339')
-    infoWindow.option_add('*Label.Ipady',10)
-    infoWindow.option_add('*Label.Font',('Frutiger Bold Regular',12,'bold'))
-    titel = Label(master=infoWindow, text='De actuele vertrektijden van station '+station+' is', font=('Frutiger Bold Regular',16,'bold'), bg='#000066', fg='white')
-    titel.grid(row=0, columnspan=7)
 
-    Label(master=infoWindow, text='EindBestemming').grid(row=1, column=0, ipadx=10, ipady=10, )
-    Label(master=infoWindow, text='Vertrekspoor').grid(row=1, column=1, ipadx=10, ipady=10, )
-    Label(master=infoWindow, text='Vertrektijd').grid(row=1, column=2, ipadx=10, ipady=10, )
-    Label(master=infoWindow, text='Vertraging').grid(row=1, column=3, ipadx=10, ipady=10, )
-    Label(master=infoWindow, text='Treinsoort').grid(row=1, column=4, ipadx=10, ipady=10, )
-    Label(master=infoWindow, text='Vervoerder').grid(row=1, column=5, ipadx=10, ipady=10, )
-    Label(master=infoWindow, text='Routetekst').grid(row=1, column=6, ipadx=10, ipady=10, )
-    Label(master=infoWindow, text='Routetips').grid(row=1, column=7, ipadx=10, ipady=10, )
-    Label(master=infoWindow, text='Opmerkingen').grid(row=1, column=8, ipadx=10, ipady=10, )
+    infoWindow["bg"]='#fed339'
+    infoWindow.option_add('*Label.Background', 'white')
+    infoWindow.option_add('*Label.Font',('Frutiger Bold Regular',12,'bold'))
+
+    titelFrame = Frame(master=infoWindow,width=1280, height=100, pady=10,bg='#fed339')
+    titel = Label(master=titelFrame, text='De actuele vertrektijden van station '+station+' is', font=('Frutiger Bold Regular',26,'bold'), bg='#fed339', fg='#000066')
+    titel.grid(row=0, columnspan=7)
+    titelFrame.grid(row=0)
+
+    infoFrame = Frame(master=infoWindow, width=1000, height=500, pady=10,bg='white')
+    Label(master=infoFrame, text='EindBestemming').grid(row=1, column=0, ipadx=10, ipady=10, )
+    Label(master=infoFrame, text='Vertrekspoor').grid(row=1, column=1, ipadx=10, ipady=10, )
+    Label(master=infoFrame, text='Vertrektijd').grid(row=1, column=2, ipadx=10, ipady=10, )
+    Label(master=infoFrame, text='Vertraging').grid(row=1, column=3, ipadx=10, ipady=10, )
+    Label(master=infoFrame, text='Treinsoort').grid(row=1, column=4, ipadx=10, ipady=10, )
+    Label(master=infoFrame, text='Vervoerder').grid(row=1, column=5, ipadx=10, ipady=10, )
+    Label(master=infoFrame, text='Routetekst').grid(row=1, column=6, ipadx=10, ipady=10, )
+    Label(master=infoFrame, text='Routetips').grid(row=1, column=7, ipadx=10, ipady=10, )
+    Label(master=infoFrame, text='Opmerkingen').grid(row=1, column=8, ipadx=10, ipady=10, )
 
     count =0
     for trein in gegevens:
@@ -65,20 +70,27 @@ def info(station):
             elif key == 'VertrekVertragingTekst':
                 continue
             if kolom == 1:
-                Label(master=infoWindow, text='{}'.format(trein[key]['#text'])).grid(row=rij, column=kolom, ipadx=10, ipady=10)
+                Label(master=infoFrame, text='{}'.format(trein[key]['#text'])).grid(row=rij, column=kolom, ipadx=10, ipady=10)
             elif kolom == 2:
                 tijd = str(trein[key])
                 tijd = tijd[11:16]
-                Label(master=infoWindow, text='{}'.format(tijd)).grid(row=rij, column=kolom, ipadx=10, ipady=10)
+                Label(master=infoFrame, text='{}'.format(tijd)).grid(row=rij, column=kolom, ipadx=10, ipady=10)
+            elif kolom == 3:
+                vertraging = '+'+str(trein[key])[2:]
+                Label(master=infoFrame, text='{}'.format(vertraging)).grid(row=rij, column=kolom, ipadx=10, ipady=10)
             else:
-                Label(master=infoWindow, text='{}'.format(trein[key])).grid(row=rij, column=kolom, ipadx=10, ipady=10)
+                Label(master=infoFrame, text='{}'.format(trein[key])).grid(row=rij, column=kolom, ipadx=10, ipady=10)
         rij += 1
         count +=1
+    infoFrame.grid(row=1,padx=100)
 
-    gaterug = Button(master=infoWindow, text='ga terug', command=stoppen, bg='#000066', fg='white', font=('Frutiger Bold Regular', 40, 'bold'))
+    buttonFrame = Frame(master=infoWindow, width=1280, height=100, pady=10,bg='#fed339')
+    gaterug = Button(master=buttonFrame, text='ga terug', command=stoppen, bg='#000066', fg='white', font=('Frutiger Bold Regular', 16, 'bold'))
     gaterug.grid(pady=10, padx=10,column=3,  row=rij)
-    gadoor = Button(master=infoWindow, text='ga door', command=choice, bg='#000066', fg='white', font=('Frutiger Bold Regular', 40, 'bold'))
+    gadoor = Button(master=buttonFrame, text='ga door', command=choice, bg='#000066', fg='white', font=('Frutiger Bold Regular', 16, 'bold'))
     gadoor.grid(pady=10, padx=10,  column=4, row=rij)
+    buttonFrame.grid(row=2)
+
     infoWindow.mainloop()
 
 
